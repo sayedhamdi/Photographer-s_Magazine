d78eb593060424366bf0ba81abe49d69dca5f3dd
unknown <ahmedjbeli54@yahoo.fr>
2019-11-17 20:07:55 +0100

    fix source integrity error

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Pulchart Magazine</title>
-    <link rel="stylesheet" href="css/styles/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles/style.css">
+    <link  rel="stylesheet" href="css/styles/bootstrap.min.css" integrity="" crossorigin="anonymous">
+    <link  rel="stylesheet" href="css/styles/style.css" integrity=""> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
--- index.php
+++ index.php
@@ -104,9 +104,9 @@
   <?php include "./includes/footer.php" ?>
 
   </body>
-  <script src="js/ajax.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="js/poper.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="js/ajax.js" integrity="" crossorigin="anonymous"></script>
+<script src="js/poper.js" integrity="" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
 <!-- our files -->
 <script type="text/javascript" src="js/main.js"></script>
 </html>
