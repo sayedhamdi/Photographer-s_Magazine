f480e14d94d7dae0dc86eef23b2f3f7a066c5b30
unknown <ahmedjbeli54@yahoo.fr>
2019-11-17 22:15:30 +0100

    commit archive page

--- archive.php
+++ archive.php
@@ -1,7 +1,53 @@
 <!-- Header -->
 <?php include "./includes/header.php" ?>
+<body>
+    <div class="container">
+        <div class="archive-date">
+            <span >    
+                The 17th of November 2019
+            </span>
+            
+            <hr class="archive-sperator-line">
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Archive/DSC_0027.jpg'/>
+                <p class='Nexa-light'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Archive/DSC_0958.jpg'/>
+                <p class='Nexa-light'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Archive/IMG_2385.jpg'/>
+                <p class='Nexa-light'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Archive\IMG_5874.jpeg'/>
+                <p class='Nexa-light'>
+                    Archive Picture 4
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Archive\sexy nutella.jpg'/>
+                <p class='Nexa-light'>
+                    Archive Picture 5
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+</body>
     
-    <br><br><br>
     
 <!-- footer start -->
 <?php include "./includes/footer.php" ?>
