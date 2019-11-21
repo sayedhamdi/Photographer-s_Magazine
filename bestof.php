52fbeecadbdf57d4347a9907914efc99c0447a20
hamza <hamza@PC-HP>
2019-11-21 23:21:19 +0100

    Finish

--- bestof.php
+++ bestof.php
@@ -4,7 +4,7 @@
 <div class="row text-center" style="padding : 100px">
     <?php for ( $i=0 ; $i<=2 ; $i++ ) { ?>
         <div class="col-md-4">
-                <img src="./img/Pages/Photography/Best of/Hanen (1).jpg" width="90%">
+                <img src="./img/Pages/Photography/Best of/Hanen (1).jpg" width="90%" height="100%">
                 <p> Hanen </p>
         </div>
     <?php } ?>
