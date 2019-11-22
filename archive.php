005cd458b57e937b089edb480308e32f594062e0
Sayed Hamdi <tkdsayed@gmail.com>
2019-11-22 19:09:41 +0100

    making the archive picture get added dynamically from folder

--- archive.php
+++ archive.php
@@ -1,5 +1,6 @@
 <!-- Header -->
 <?php include "./includes/header.php" ?>
+
 <body>
     <div class="container">
         <div class="archive-date">
@@ -9,41 +10,34 @@
 
             <hr class="archive-sperator-line">
         </div>
+        <?php
+        $dir =
+
+        $directory = './img/Pages/Photography/Archive';
+        $scanned_directory = scandir($directory);
+        for ($i=2;$i<sizeof($scanned_directory);$i++){
+        ?>
         <div class="row">
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive/DSC_0027.jpg'/>
+                <img class="img-fluid" src='<?php echo $directory.'/'.$scanned_directory[$i]?> '/>
                 <p class='Nexa-light'>
                     Archive Picture 1
                 </p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive/DSC_0958.jpg'/>
+                <img class="img-fluid" src='<?php echo $directory.'/'.$scanned_directory[$i+1]?>'/>
                 <p class='Nexa-light'>
                     Archive Picture 2
                 </p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive/IMG_2385.jpg'/>
+                <img class="img-fluid" src='<?php echo $directory.'/'.$scanned_directory[$i+2]?>'/>
                 <p class='Nexa-light'>
                     Archive Picture 3
                 </p>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive\IMG_5874.jpeg'/>
-                <p class='Nexa-light'>
-                    Archive Picture 4
-                </p>
-            </div>
-            <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive\sexy nutella.jpg'/>
-                <p class='Nexa-light'>
-                    Archive Picture 5
-                </p>
-            </div>
-        </div>
-
+      <?php $i+=3;}?>
     </div>
 
 </body>
