05372ae797e8a0d185f9c31afad2e81210f867c4
Sayed Hamdi <tkdsayed@gmail.com>
2019-11-23 00:00:49 +0100

    adding submission.php and it's style from oussema

--- submission.php
+++ submission.php
@@ -1,6 +1,153 @@
 <!-- Header -->
     <?php include "./includes/header.php" ?>
-    
+
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+        <div class="contenuu">
+        <div class="sub_para">
+        <pre>
+
+                                      PLEASE NOTE
+
+         We occasionally accept submissions from Photographers, Videographers, Stylists,
+         Makeup Artists, Agencies, Models, Hairstylists & Creative
+         Director. You must be the copyright holder to the images being submitted,
+         or have permission from the copyright holder to submit the images. Portfolio
+         is only for the photographers thats why we dont make portfolio to anyone that
+         is not photographer. We do not pay for submission or pubblication of fill or
+         partial content of any kind. Any content you submit for EDITORIAL  must not be
+         previously published in any other magazine or platform including social
+         network. We reserved the right to pull your story form our website and social
+          without your consent.
+        </pre>
+        </div>
+        <div class="ligne"></div>
+
+
+
+        <div class="sub_para">
+        <pre>
+
+                                                 - PORTFOLIO -
+
+         We would like to remind you that you can submit  photos every Wednesday from 12PM to 6PM GMT+1
+         Requirements:
+         1 - 2 images MAX
+        JPG or TIF format only
+        At least 6500 pixels 300dpi  *OPTIONAL
+        Credits should contain everyone’s INSTAGRAM tags.
+        Images must be  NON-EXCLUSIVE or EXCLUSIVE
+        ⚠️English names text only.
+        ⚠️Credits should contain everyone’s INSTAGRAM tags.
+        ⚠️ Submitting more than 2 photos will automatically declined without explanation.
+        </pre>
+        </div>
+
+        <div class="portfolio">
+        <a href="#">
+        <div class="container">
+          <h2>Web | portfolio submission</h2>
+          <span class="description">Call for work</span>
+          <span class="deadline">Deadline:27 December</span>
+        </div>
+        </a>
+        <div class="btn"><button>close</button></div>
+        </div>
+        <div class="ligne"></div>
+
+        <div class="sub_para">
+        <pre>
+                                                   - EXCLUSIVE -
+
+        We would like to remind you that you can submit EXCLUSIVE photos every SATURDAY  from 12PM to 6PM GMT+1
+        Requirements:
+        1 - 2 images  MAX
+        JPG or TIF format only
+        At least 6500 pixels 300dpi  *OPTIONAL
+        ⚠️English names text only.
+        ⚠️Credits should contain everyone’s INSTAGRAM tags.
+        ⚠️Images must be  EXCLUSIVE and UNPUBLISHED
+        ⚠️ Submitting more than 2 photos will automatically declined without explanation
+        </pre>
+        </div>
+        <div class="portfolio">
+        <a href="#">
+        <div class="container">
+          <h2>Web | portfolio submission</h2>
+          <span class="description">Call for work</span>
+          <span class="deadline">Deadline:27 December</span>
+        </div>
+        </a>
+        <div class="btn"><button>close</button></div>
+        </div>
+
+        <div class="ligne"></div>
+
+        <div class="sub_para">
+        <pre>
+
+         - BEAUTY -
+
+        Requirements:
+        Title
+        10 - 15 images
+        4 - 10 looks
+        JPG or TIF format
+        At least 6500 pixels 300dpi  *OPTIONAL
+       ⚠️English names text only.
+       ⚠️Credits should contain everyone’s INSTAGRAM tags.
+       ⚠️Images must be EXCLUSIVE and UNPUBLISHED
+      </pre>
+      </div>
+
+      <div class="portfolio">
+        <a href="#">
+        <div class="container">
+          <h2>Web | portfolio submission</h2>
+          <span class="description">Call for work</span>
+          <span class="deadline">Deadline:27 December</span>
+        </div>
+        </a>
+        <div class="btn"><button>close</button></div>
+        </div>
+        <div class="ligne"></div>
+
+        <div class="sub_para">
+        <pre>
+          - FASHION -
+        Requirements:
+        Title
+        10 - 15 images
+        4 - 10 looks
+       JPG or TIF format
+       At least 6500 pixels 300dpi  *OPTIONAL
+      ⚠️English names text only.
+      ⚠️Credits should contain everyone’s INSTAGRAM tags.
+      ⚠️Images must be  EXCLUSIVE and UNPUBLISHED
+
+       </pre>
+       </div>
+       <div class="portfolio">
+        <a href="#">
+        <div class="container">
+          <h2>Web | portfolio submission</h2>
+          <span class="description">Call for work</span>
+          <span class="deadline">Deadline:27 December</span>
+        </div>
+        </a>
+        <div class="btn"><button>close</button></div>
+        </div>
+        <div class="ligne"></div>
+
+        </div>
+      </body>
+    </html>
+
 <br><br><br>
 
 <!-- footer start -->
