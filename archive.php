c881bd6b3363bcbfee22ce0da809d250700b3cb8
unknown <ahmedjbeli54@yahoo.fr>
2019-11-22 19:27:28 +0100

    add images to archive

--- archive.php
+++ archive.php
@@ -1,45 +1,335 @@
 <!-- Header -->
 <?php include "./includes/header.php" ?>
+
 <body>
     <div class="container">
         <div class="archive-date">
-            <span >    
+            <span>
                 The 17th of November 2019
             </span>
-            
+
             <hr class="archive-sperator-line">
         </div>
         <div class="row">
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive/DSC_0027.jpg'/>
-                <p class='Nexa-light'>
+                <img class="img-fluid" src='img\Pages\Archive\1.jpeg' />
+                <p class='Nexa-light img-title'>
                     Archive Picture 1
                 </p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive/DSC_0958.jpg'/>
-                <p class='Nexa-light'>
+                <img class="img-fluid" src='img\Pages\Archive\2.jpeg' />
+                <p class='Nexa-light img-title'>
                     Archive Picture 2
                 </p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive/IMG_2385.jpg'/>
-                <p class='Nexa-light'>
+                <img class="img-fluid" src='img\Pages\Archive\3.jpeg' />
+                <p class='Nexa-light img-title'>
                     Archive Picture 3
                 </p>
             </div>
         </div>
         <div class="row">
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive\IMG_5874.jpeg'/>
-                <p class='Nexa-light'>
+                <img class="img-fluid" src='img\Pages\Archive\4.jpeg' />
+                <p class='Nexa-light img-title'>
                     Archive Picture 4
                 </p>
             </div>
             <div class="col-md-4">
-                <img class="img-fluid" src='img\Archive\sexy nutella.jpg'/>
-                <p class='Nexa-light'>
+                <img class="img-fluid" src='img\Pages\Archive\5.jpg' />
+                <p class='Nexa-light img-title'>
                     Archive Picture 5
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\6.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 5
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\7.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\8.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\9.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\10.jpg' />
+                <p class='Nexa-light img-title img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\11.jpg' />
+                <p class='Nexa-light img-title img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\12.jpg' />
+                <p class='Nexa-light img-title img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\13.jpg' />
+                <p class='Nexa-light img-title img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\14.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\15.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\16.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\17.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\18.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\19.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\20.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\21.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\22.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\23.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\24.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\25.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\26.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\27.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\28.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\29.jpeg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\30.jpeg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\31.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\32.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\33.jpeg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\34.jpeg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\35.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\36.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\37.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\38.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\39.jpeg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\40.jpeg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\41.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\42.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\43.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\44.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\45.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 3
+                </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\46.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 1
+                </p>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src='img\Pages\Archive\47.jpg' />
+                <p class='Nexa-light img-title'>
+                    Archive Picture 2
                 </p>
             </div>
         </div>
@@ -47,7 +337,7 @@
     </div>
 
 </body>
-    
-    
+
+
 <!-- footer start -->
 <?php include "./includes/footer.php" ?>
