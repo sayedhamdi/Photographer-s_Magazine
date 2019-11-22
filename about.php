cd2ab6456c88370d684e17a495e3452bb2fe7126
unknown <ahmedjbeli54@yahoo.fr>
2019-11-22 21:36:00 +0100

    minor fix

--- about.php
+++ about.php
@@ -2,22 +2,32 @@
 <?php include "./includes/header.php" ?>
 <br><br><br>
 <!-- about us section -->
-   <h2 class="text-center Nexa-Bold">ABOUT</h2>
+<h2 class="text-center Nexa-Bold">ABOUT</h2>
 
 
 <div class="container">
   <div class="row">
     <div class="col-12 col-sm-12">
-      <p class="Nexa-Light">
-      <b>PULCHART is a combination between two latin words</b>.<br> Pulchra, this word means Fine and beautiful and Artem means art SO PULCHART means: the beauty of art.
+      <p class="Nexa-Light" style="
+    padding-top: 25px;
+    text-align: center;">
+        <b>PULCHART is a combination between two latin words</b>.<br> Pulchra, this word means Fine and beautiful and
+        Artem means art SO PULCHART means: the beauty of art.
 
-      <br><br><b>PULCHART</b> magazine is a pure artistic portrait photography magazine Specialized in Editorial fashion and beauty, conceptual, fine art and fantasy portrait photography.
+        <br><br><b>PULCHART</b> magazine is a pure artistic portrait photography magazine Specialized in Editorial
+        fashion and beauty, conceptual, fine art and fantasy portrait photography.
 
-      <br>The magazine goal is to build a huge community, to gather photographers, fashion designers, make-up artists and models to work with us on the aim of changing the general taste of Tunisian people about portrait photography.
+        <br>The magazine goal is to build a huge community, to gather photographers, fashion designers, make-up artists
+        and models to work with us on the aim of changing the general taste of Tunisian people about portrait
+        photography.
 
-      <br>Also we seek to make our photos and articls a great source of learning and inspiration because behind each project, there is an article that has the inspiration, aesthetics and the way how the team made their artistic master pieces.
+        <br>Also we seek to make our photos and articls a great source of learning and inspiration because behind each
+        project, there is an article that has the inspiration, aesthetics and the way how the team made their artistic
+        master pieces.
 
-      <br>Furthermore, PULCHART shares with you some success stories of amazing artists (Photographers, fashion designers, make-up artists and models) to encourage people to follow their desires and to achieve their dreams out and reach them.
+        <br>Furthermore, PULCHART shares with you some success stories of amazing artists (Photographers, fashion
+        designers, make-up artists and models) to encourage people to follow their desires and to achieve their dreams
+        out and reach them.
       </p>
     </div>
 
