a453e226c5adba9d0aa47fcb880f20c24189157a
unknown <ahmedjbeli54@yahoo.fr>
2019-12-01 22:02:09 +0100

    exclusive row full width

--- index.php
+++ index.php
@@ -42,16 +42,15 @@
       <h1 class="text-center Nexa-Bold">Exclusive</h1>
     </div>
   </div>
-  <div class="offset-md-0 offset-lg-2">
     <div class="row ">
-      <div class="col-12 col-lg-4  col-md-6 col-sm-12">
+      <div class="col-12 col-lg-6  col-md-6 col-sm-12">
         <img src="img/Pages/Acceuil/Hanen BEST OF.jpg" class="img-fluid" />
         <p class="Nexa-Bold">
           Picture of the week
         </p>
       </div>
 
-      <div class="col-12 col-lg-4 col-md-6  col-sm-12">
+      <div class="col-12 col-lg-6 col-md-6  col-sm-12">
 
         <div class="row">
           <div class="col">
