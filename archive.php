5fa999f09d090f648bbf179c1eef18c20b6f53ad
unknown <ahmedjbeli54@yahoo.fr>
2019-12-04 11:28:35 +0100

    minor fix

--- archive.php
+++ archive.php
@@ -13,16 +13,17 @@
         </div>
         <div class="row text-center">
         <?php
-            $res = mysqli_query($conn,'select dis,name from image,photographe where image.id = photographe ');
+            $res = mysqli_query($conn,'select dis,photographe.firstname,photographe.lastname from photographe,image 
+            where photographe.id = image.owner');
             $i=1;
             while($row = mysqli_fetch_array($res)){ 
         ?>
             <div class="col-md-4">
-                <a href='<?php echo $row['dir']?> ' target="_blank">
-                    <img class="img-responsive lazyestload" src='<?php echo $row['dir']?> ' />
+                <a href='<?php echo $row['dis']?> ' target="_blank">
+                    <img class="img-responsive lazyestload" src='<?php echo $row['dis']?> ' />
                 </a>
                 <h5>
-                <?php echo $row['name']?>
+                <?php echo "".$row['firstname']." ". $row['lastname']?>
                 </h5>
             </div>
         <?php if($i % 3 == 0) echo('</div>
