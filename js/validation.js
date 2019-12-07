50853fb037a3491cfe33526792fecbd807a891df
unknown <ahmedjbeli54@yahoo.fr>
2019-12-07 16:01:33 +0100

    add todo :p

--- js/validation.js
+++ js/validation.js
@@ -3,6 +3,9 @@ const RegexAlphab = /^[a-zA-Z]*$/
 const RegexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 const RegexDate = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/
 
+//todo reinit errors && check length && fix span append
+// add helpers and placeholer
+
 function validate(){
     const fname = document.getElementById('firstname')
     const lname = document.getElementById('lastname')
@@ -17,12 +20,12 @@ function validate(){
         errors['firstname'] = 'please enter a valid First name';
     }
     
-    if (!RegexAlphab.test(lname) ){
+    if (!RegexAlphab.test(lname.value) ){
         errors['lastname'] = 'please enter a valid Last name';
 
     }
 
-    if (!RegexDate.test(lname) ){
+    if (!RegexDate.test(date.value) ){
         errors['date'] = 'please enter a valid BirthDate';
 
     }
@@ -32,7 +35,6 @@ function validate(){
             var errorNode = document.createElement("span"); 
             var errorContent = document.createTextNode(errors[key]); 
             errorNode.appendChild(errorContent);  
-            alert(key)
             var errorInput = document.getElementById(key); 
             errorInput.style.borderColor = "crimson"
             errorInput.parentNode.insertBefore(errorNode, errorInput.nextSibling);
