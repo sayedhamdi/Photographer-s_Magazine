1ae348c3b722ef90347cf0ba0579edc48249e606
unknown <ahmedjbeli54@yahoo.fr>
2019-12-11 16:19:03 +0100

    login backend

--- includes/login.php
+++ includes/login.php
@@ -1,5 +1,6 @@
 <?php
-session_start(); 
+
+if (session_status() == PHP_SESSION_NONE) session_start(); 
 
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: index.php");
@@ -29,35 +30,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if($valid){
         $stmt =  $conn->prepare("SELECT id, email, password FROM photographe WHERE email = ?") ; 
-
-        if($stmt){
-            $stmt->bind_param('s',$email);
-
-            if($stmt->execute()){
-                $stmt->store_result();
-                
-                // if email exists verify password
-                if($stmt->num_rows() == 1){                    
-                    $stmt->bind_result('sss',$id, $email, $hashed_password);
-                    if($row = $stmt->fetch()){
-                        if(password_verify($password, $row['password'])){
-                            session_start();
-                            
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["email"] = $email;                            
-                            
-                            header("location: index.php");
-                        } else{
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    $email_err = "No account found with that username.";
+        $stmt->bind_param('s',$email);
+        $stmt->execute();
+        $result = $stmt->store_result();
+        if($stmt->num_rows() == 1){ 
+            if($row = $result->fetch_assoc()){
+                if(password_verify($password, $row['password'])){
+                    session_start();
+            
+                    $_SESSION["loggedin"] = true;
+                    $_SESSION["id"] = $id;
+                    $_SESSION["email"] = $email;                            
+                    
+                    header("location: index.php");
                 }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                else $password_err = "The password you entered was not valid.";
+
             }
+            else $email_err = "No account found with that email.";
         }
     }
 }
