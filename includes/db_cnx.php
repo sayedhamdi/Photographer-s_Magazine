a1986b98a4c0af1e89a8f1eb6aa7313bcbaec224
Sayed Hamdi <tkdsayed@gmail.com>
2019-12-13 19:24:21 +0100

    changing credentials to db

--- includes/db_cnx.php
+++ includes/db_cnx.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "pulchart_admin";
+    $password = "pulchartAdmin123";
     $db_name = "pulchart_pulchart";
 
     // Create connection
