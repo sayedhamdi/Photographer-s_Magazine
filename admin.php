eb372229efa6e0f5e21c1bf31ca4fe19ba5b1562
unknown <ahmedjbeli54@yahoo.fr>
2019-12-19 12:49:03 +0100

    minor fix

--- admin.php
+++ admin.php
@@ -5,37 +5,39 @@
 ?>
 
 <form action="checkbox.php" method="POST">
-    <div id="checkdivs">
+    <div class="checkdivs">
         <?php
             $getAll = mysqli_query($conn,"select name,category,photographe.firstname,photographe.lastname,owner,image.id from image,photographe where
-                photographe.id = owner and verified = 0 limit 5");
+                photographe.id = owner and verified = 0 limit 7");
 
     if (mysqli_num_rows($getAll) > 0){
         while($row = mysqli_fetch_array($getAll)){    
                 echo '
-                    <div class=checkdiv id="'.$row['id'].'">
-                    <div class="img-div" style="background-image:url('.$row['name'].')"></div>
-                    <div class=img-detail>
-                    <a class=expand href='.$row['name'].' target=_blank>
-                    <img src="img/icons/expand.svg">
-                    </a>
-                        <span class=category>#fashion</span>
-                        <span class=photographe>'.$row['firstname'].' '.$row['lastname'].'</span>
-                        <h5 class=date><span style="font-family:Nexa-Bold">submitted:</span> 20-12-2019</h5>
-                        
-                        <div class="action-btns">
-                        <input type="checkbox" name="'.$row['id'].'" value="'.$row['id'].'" id="add-'.$row['id'].'"  />
-                        <input type="checkbox" name="'.$row['id'].'" value="'.$row['id'].'" id="decline-'.$row['id'].'"  />
-                        <label class="btn btn-add  " style="" for="add-'.$row['id'].'">+ add</label>
-                        <label class="btn btn-decline" style="" for="decline-'.$row['id'].'">x decline</label>
+                        <div class="checkdiv" id="'.$row['id'].'">
+                            <div class="img-div" style="background-image:url('.$row['name'].')">
+                            </div>
+                            
+                            <div class=img-detail>
+                                <a class=expand href='.$row['name'].' target=_blank>
+                                    <img src="img/icons/expand.svg">
+                                </a>
+                                    <span class=category>#fashion</span>
+                                    <span class=photographe>'.$row['firstname'].' '.$row['lastname'].'</span>
+                                    <h5 class=date><span style="font-family:Nexa-Bold">submitted:</span> 20-12-2019</h5>
+                                    
+                                    <div class="action-btns">
+                                        <input type="checkbox" name="'.$row['id'].'" value="'.$row['id'].'" id="add-'.$row['id'].'"  />
+                                        <input type="checkbox" name="'.$row['id'].'" value="'.$row['id'].'" id="decline-'.$row['id'].'"  />
+                                        <label class="btn btn-add  " style="" for="add-'.$row['id'].'">+ add</label>
+                                        <label class="btn btn-decline" style="" for="decline-'.$row['id'].'">x decline</label>
+                                    </div>
+                            </div>
                         </div>
-    
-                    </div>
-                    </div>
                 ';
         }
      }
     ?>
+    </div>
 </form>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js">
 </script>
